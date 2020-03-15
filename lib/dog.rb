85c2439eae73fdc14805b712d0a173d058138dbc
Jessica Smith <jessikahd.smith@gmail.com>
2020-03-15 18:12:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
-# Add your code here
+class Dog 
+def initialize(name)
+    @name = name
+   save 
+  end
+
+  
+  def self.all
+  end
+    
+    def self.print_all
+    end
+      
+      def self.clear_all
+         @@all.clear
+      end
+    
+
+  def save
+    @@all << self
+  end
+end
